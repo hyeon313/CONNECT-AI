2c5f3d3401aad7dd02041890cb5c32725217535e
JiHyeon Lee <54384248+hyeon313@users.noreply.github.com>
2020-09-12 14:23:55 +0900

    Update test_merge_ImageAnnotationTool.py

--- test_merge_ImageAnnotationTool.py
+++ test_merge_ImageAnnotationTool.py
@@ -16,26 +16,34 @@
 # 마스크된 좌표를 1로 표시한 numpy array를 CSV파일로 저장하기 완료
 
 
+# 9.12. 
+# ju_jh_ym 합치는 중
+
+
 
 
 import sys
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import (QApplication, QLabel, QMainWindow, QWidget, QHBoxLayout,\
-     QVBoxLayout, QAction, QFileDialog, QGraphicsView, QGraphicsScene)
-from PyQt5.QtGui import QPixmap, QIcon, QImage
-from PyQt5.QtCore import Qt
+     QVBoxLayout, QAction, QFileDialog, QGraphicsView, QGraphicsScene, QCheckBox, QComboBox, QPushButton)
+from PyQt5.QtGui import QPixmap, QIcon, QImage, QWheelEvent, QPainter, QPen, QBrush
+from PyQt5.QtCore import Qt, QPoint
 from PyQt5.QtWidgets import *
-import pydicom
-
+# import Utility
+import pydicom # DICOM파일(국제 표준에 따라 생성된 메디컬 이미지 파일)을 
+               # 가져오기 위해 python packages 중 하나인 pydicom을 import
 import numpy as np
-import SimpleITK as itk #dicom 관련
-import qimage2ndarray
+import SimpleITK as itk # SimpleITK로부터 itk(이미지 분할 및 이미지 등록 플랫폼)을 import
+import qimage2ndarray # Qimages와 numpy.ndarrays를 빠르게 변환하기위해 qimage2ndarray를 import
 import math
 
 class MyWidget(QWidget): 
     def __init__(self): 
         super().__init__() 
 
+        # self.lbl_original_img = QLabel()
+        # self.lbl_blending_img = QLabel()
+        # self.lbl_original_img.setStyleSheet("border-style: solid;""border-width: 2px;")
         self.lbl_original_img = QGraphicsScene()
         self.lbl_blending_img = QGraphicsScene()
         self.view_1 = QGraphicsView(self.lbl_original_img) 
@@ -43,20 +51,35 @@ class MyWidget(QWidget):
         self.view_1.setFixedSize(514, 514)
         self.view_2.setFixedSize(514, 514)
 
+        self.addMaskBtn = QPushButton('&Add Mask', self)
+        self.maskComboBox = QComboBox(self)
+        self.drawCheckBox = QCheckBox('Draw', self)
+        self.maskCheckBox = QCheckBox('Mask on', self)
+        self.blendCheckBox = QCheckBox('Blended Mask on', self)
+
         self.lbl_pos = QLabel()
         self.lbl_pos.setAlignment(Qt.AlignCenter)
         
-        self.hbox = QHBoxLayout()
+        self.hViewbox = QHBoxLayout()
+        self.hViewbox.addWidget(self.view_1)
+        self.hViewbox.addWidget(self.view_2)
 
-        self.hbox.addWidget(self.view_1)
-        self.hbox.addWidget(self.view_2)
+        self.hOptionbox = QHBoxLayout()
+        self.hOptionbox.addWidget(self.addMaskBtn)
+        self.hOptionbox.addWidget(self.maskComboBox)
+        self.hOptionbox.addWidget(self.drawCheckBox)
+        self.hOptionbox.addWidget(self.maskCheckBox)
+        self.hOptionbox.addWidget(self.blendCheckBox)
         
         self.vbox = QVBoxLayout()
-        self.vbox.addLayout(self.hbox)
+        self.vbox.addLayout(self.hViewbox)
+        self.vbox.addLayout(self.hOptionbox)
         self.vbox.addWidget(self.lbl_pos)
         
         self.setLayout(self.vbox)
-
+#         self.setGeometry(300, 100, 350, 150) # x, y, width, height 
+#         self.setWindowTitle("QWidget") 
+#         self.show()
 
 class MyApp(QMainWindow):
 
@@ -64,21 +87,21 @@ class MyApp(QMainWindow):
         super().__init__()
         
         #지현
-        # self.window_level = 40
-        # self.window_width = 400
-        self.LRpoint = [0, 0]
-        self.LRClicked = False #두 버튼 모두 눌림 여부 확인
-        self.window_level = 0
-        self.window_width = 0
+        self.LRpoint = [0, 0] # window_level, window_width설정을 위한 기준점을 0으로 설정
+        self.LRClicked = False # 두 버튼 모두 눌림 여부 확인 / 마우스 동시 클릭을 토글로 설정하기 위한 전 처리
+        self.window_level = 40 # 기준으로 값을 넣음
+        self.window_width = 400 # 기준으로 값을 넣음
         self.deltaWL = 0
         self.deltaWW = 0
-
 
 
         #종엄
         self.Nx = 0 #이미지 x차원
         self.Ny = 0 #이미지 y차원
         self.NofI = 0 #이미지 개수
+
+        # 전체 이미지 정보
+
 
         self.cur_idx = 0 #현시점 띄워줄 이미지 
         self.cur_image = [] #현재 선택된 한장의 이미지
@@ -98,44 +121,80 @@ class MyApp(QMainWindow):
         
         
     def initUI(self):
-        
+        # file open Action===> 툴바로부터 파일을 여는 액션을 생성
         openAction = QAction(QIcon('exit.png'), 'Open', self)
         openAction.triggered.connect(self.openImage)
         self.toolbar = self.addToolBar('Open')
         self.toolbar.addAction(openAction)
-
+        
+        # dialog Button===> 원하는 이미지 번호를 받을 dialog 생성
+        Dbtn = QPushButton('&ImgNum', self)
+        Dbtn.move(980, 565)
+        Dbtn.setCheckable(True)
+        Dbtn.toggle()
+        Dbtn.clicked.connect(self.showDialog)
+        # previous button===> 이전 이미지로 넘어가기위한 Button 생성       
         btn1 = QPushButton('&previous', self)
-        btn1.move(150, 565)
+        btn1.move(700, 565)
         btn1.setCheckable(True)
         btn1.toggle()
-
+        # next button===> 다음 이미지로 넘어가기위한 Button 생성
         btn2 = QPushButton('&next', self)
         btn2.move(800, 565)
-
+        btn1.setCheckable(True)
+        btn1.toggle()
+        # 단축키
         btn1.setShortcut('Ctrl+1')
         btn2.setShortcut('Ctrl+2')
-
+        #btn1과 btn2에 각각 ~_clicked 연결하여 이벤트 발생
         btn1.clicked.connect(self.btn1_clicked)
         btn2.clicked.connect(self.btn2_clicked)
         
         self.setWindowTitle('Test Image')
 
         self.setGeometry(300, 300, 1100, 600)
+
         self.show()
 
-    #종엄
-    def btn1_clicked(self):
-        #QMessageBox.about(self, "메세지", "이전")
-        self.cur_idx = self.cur_idx - 1
-                
-        if self.cur_idx < 0:
-            self.cur_idx = 0
+    #Dbtn이 버튼 클릭으로부터 호출 할 showDialog함수 선언
+    def showDialog(self):
+        num, ok = QInputDialog.getInt(self, 'Input ImageNumber', 'Enter Num') #QInputDialg클래스에 getInt매소드를 이용하여 입력값을 튜플 형태로 num과ok에 넣기
+        #self.label.setText("text: %s" % (text))
+        self.cur_idx = num - 1 # 입력받은 num값을 cur_idx(인덱스)에 넣는다(0부터 시작해서 입력값에 1빼기)
+        # if ok:
+        #     self.label.setText(str(num))
+        print("show image",self.cur_idx + 1)
         if self.cur_idx > self.NofI-1:
-            self.cur_idx = self.NofI-1
-            
-        print("left", self.cur_idx)
+            self.cur_idx = self.NofI-1 # num-1값이 NofI(이미지 전체 개수)를 넘어가면 마지막값[223]을 계속 가지게 만든다
+        elif self.cur_idx < 0:
+            self.cur_idx = self.NofI-224
+
+        ###########질문########
+        # if self.cur_idx == -1: #self.cur_idx가 -1,즉 num이 0을 받아온 상황에서의 조건 생성 중....
+        #     QMessageBox.about(self, "메세지", "no")
+
+        
+        self.cur_image = self.EntireImage[self.cur_idx] #cur_idx값을 EntireImage를 통해 cur_image에 넣기
+        
+        image = self.AdjustPixelRange(self.cur_image, self.window_level, self.window_width) #지현
+        # cur_image와 window_level, window_width를 AdgustPixelRange함수에 통과시켜 image에 넣기
+        image = qimage2ndarray.array2qimage(image) # 통과시킨 imaeg를 Qimage로 전환
+        image = QPixmap.fromImage(QImage(image)) #Qimage 형태인 image를 받아 fromImage매소드를 써서 QPixmap(이미지를 보여줄때 쓰는 객체, 포맷들을 지원)
+                                #QImage=Qpaint를 사용하거나 이미지 필셀조작
+                                #QPixmap= 기존 이미지를 반복해서 그릴 때
+        self.wg.lbl_original_img.addPixmap(image) #original_img = 왼쪽 화면
+        self.wg.lbl_blending_img.addPixmap(image) #blending_img = 오른쪽 화면
+        self.wg.view_1.setScene(self.wg.lbl_original_img)
+        self.wg.view_2.setScene(self.wg.lbl_blending_img)
+        self.wg.view_1.show()
+        self.wg.view_2.show()
+
+    def onChanged(self,text):
+        self.lbl.setText(text)
+        self.lbl.adjustSize()
 
 
+    def refresh(self):
         self.cur_image = self.EntireImage[self.cur_idx]
 
         image = self.AdjustPixelRange(self.cur_image, self.window_level, self.window_width) #지현
@@ -145,24 +204,24 @@ class MyApp(QMainWindow):
 
         #왼쪽 프레임 이미지 업데이트 필요
         self.wg.lbl_original_img.addPixmap(image)
+        self.wg.lbl_blending_img.addPixmap(image)
         self.wg.view_1.setScene(self.wg.lbl_original_img)
-        self.wg.view_2.setScene(self.wg.lbl_original_img)
+        self.wg.view_2.setScene(self.wg.lbl_blending_img)
         self.wg.view_1.show()
         self.wg.view_2.show()
 
 
+
     #종엄
-    def btn2_clicked(self):
-        #QMessageBox.about(self, "메세지", "다음")
-
-        self.cur_idx = self.cur_idx + 1
-
-        if self.cur_idx < 0:
-            self.cur_idx = 0
-        if self.cur_idx > self.NofI-1:
-            self.cur_idx = self.NofI-1
-
-        print("right", self.cur_idx)
+    #btn1이 버튼 클릭으로부터 호출 할 btn1_clicked함수 선언
+    def btn1_clicked(self):
+        #QMessageBox.about(self, "메세지", "이전")
+        self.cur_idx = self.cur_idx - 1 # 현재 인덱스에 1을 빼서 cur_idx에 넣기
+                
+        if self.cur_idx < 0: 
+            self.cur_idx = 0 #0보다 작으면 0으로 남기
+            
+        print("left and image", self.cur_idx +1)
 
 
         self.cur_image = self.EntireImage[self.cur_idx]
@@ -174,8 +233,37 @@ class MyApp(QMainWindow):
 
         #왼쪽 프레임 이미지 업데이트 필요
         self.wg.lbl_original_img.addPixmap(image)
+        self.wg.lbl_blending_img.addPixmap(image)
         self.wg.view_1.setScene(self.wg.lbl_original_img)
-        self.wg.view_2.setScene(self.wg.lbl_original_img)
+        self.wg.view_2.setScene(self.wg.lbl_blending_img)
+        self.wg.view_1.show()
+        self.wg.view_2.show()
+
+    #종엄
+    #btn2이 버튼 클릭으로부터 호출 할 btn2_clicked함수 선언
+    def btn2_clicked(self):
+        #QMessageBox.about(self, "메세지", "다음")
+
+        self.cur_idx = self.cur_idx + 1 # 현재 인덱스에서 1을 더해서 cur_idx에 넣기
+
+        if self.cur_idx > self.NofI-1:
+            self.cur_idx = self.NofI-1 #223보다 크면 223으로 남기
+
+        print("right and image=", self.cur_idx +1)
+
+
+        self.cur_image = self.EntireImage[self.cur_idx]
+
+        image = self.AdjustPixelRange(self.cur_image, self.window_level, self.window_width) #지현
+        
+        image = qimage2ndarray.array2qimage(image)
+        image = QPixmap.fromImage(QImage(image))
+
+        #왼쪽 프레임 이미지 업데이트 필요
+        self.wg.lbl_original_img.addPixmap(image)
+        self.wg.lbl_blending_img.addPixmap(image)
+        self.wg.view_1.setScene(self.wg.lbl_original_img)
+        self.wg.view_2.setScene(self.wg.lbl_blending_img)
         self.wg.view_1.show()
         self.wg.view_2.show()
 
@@ -193,10 +281,7 @@ class MyApp(QMainWindow):
         return image
 
 
-    # #지현 예시
-    # def mouseEvent():
-    #     #self.EntireImage 이미지전체를 조절해줘야함.
-    #     self.adjustedImage = self.AdjustPixelRange(self.EntireImage, self.window_level, self.window_width)
+   
 
 
     
@@ -205,7 +290,7 @@ class MyApp(QMainWindow):
 
         print("open", imagePath)
 
-        folder_path = "C:\Project\label" #"C:\\Users\\yjm45\\python\\LD2HD\\L067"
+        folder_path = "C:\Project\label"
 
 
         reader = itk.ImageSeriesReader() 
@@ -221,11 +306,12 @@ class MyApp(QMainWindow):
         print(type(images[0]), type(images[1]))
 
         ImgArray = itk.GetArrayFromImage(images)   
-        self.EntireImage = np.asarray(ImgArray, dtype=np.float32)
-        self.EntireImage = np.squeeze(self.EntireImage)
+        self.EntireImage = np.asarray(ImgArray, dtype=np.float32) #입력 데이터를 ndarray로 변환하나 이미 ndarray일 경우 새로 메모리에 ndarray가 생성 (x)
+        self.EntireImage = np.squeeze(self.EntireImage)# 차원 축소
+        print(type(self.EntireImage))
         # 넘파이 이미지 (224, 512, 512)로 전체 데이터 받음.
 
-        print(self.EntireImage.shape)
+        print(self.EntireImage.shape)# (224, 512, 512)
 
         self.NofI = self.EntireImage.shape[0]  #이미지 갯수
         self.Nx = self.EntireImage.shape[1] #x차원
@@ -233,10 +319,6 @@ class MyApp(QMainWindow):
 
         self.cur_image = self.EntireImage[self.cur_idx] #현재 이미지 셋팅
 
-
-
-
-        #image = self.AdjustPixelRange(image, self.window_level, self.window_width) #지현
         image = self.AdjustPixelRange(self.cur_image, self.window_level, self.window_width) #지현
         
         image = qimage2ndarray.array2qimage(image)
@@ -265,105 +347,91 @@ class MyApp(QMainWindow):
         self.wg.lbl_pos.setText(txt)
         self.wg.lbl_pos.adjustSize()
 
+    def wheelEvent(self, event):
+        
+        n_scroll = int(event.angleDelta().y() / 120)
+        
+        self.cur_idx = self.cur_idx + n_scroll
+        if self.cur_idx < 0:
+            self.cur_idx = 0
+        if self.cur_idx > self.NofI-1:
+            self.cur_idx = self.NofI-1
+
+        print (self.cur_idx)
+        # n_scroll = int(event.angleDelta().y() /120)
+        
+        
+        # self.cur_idx = self.cur_idx +n_scroll
+        # print(self.cur_idx)
+        self.refresh()
+    
+
+    def mouseMoveEvent(self, event):
+        # 지현
         if self.LRClicked:
-           # print("after click: ", event.globalX(), event.globalY())
-
-            mX = float(event.globalX())
-            mY = float(event.globalY())
-            
-            
-
-            #조건
+            # 기준점
             rX = np.array(self.LRpoint[0]) #기준점
             rY = np.array(self.LRpoint[1])
-
-           # print(type(mX), mY, type(rX), rY)
-
-            #오른쪽부분
-            # self.window_level
-            # self.window_width
+            
+            # 현재
+            mX = event.globalX()
+            mY = event.globalY()
 
             square = (rX - mX)*(rX - mX) + (rY - mY)*(rY - mY)
             dist = math.sqrt(square)
 
-            temp_wl = 0
-            temp_ww = 0
-
-            if rX < mX: #오른쪽
-                
-                #temp_wl = self.window_level + dist
+            # window_level 설정
+            if rX < mX: # X축 기준 오른쪽 
                 self.deltaWL  = dist
                 
-            else: #왼쪽부분
-                #temp_wl = self.window_level - dist
+            else: # X축 기준 왼쪽부분
                 self.deltaWL  = -dist
 
-
-            if rY < mY: #아래부분
-                #temp_ww = self.window_width - dist
+            # window_width 설정
+            if rY < mY: # Y축 기준 아래부분     
                 self.deltaWW = -dist
 
-            else: #윗부분
-                #temp_ww = self.window_width + dist
+            else: # Y축 기준 윗부분
                 self.deltaWW = dist
 
-            #self.deltaWL, self.deltaWW 가지고 요 내부에서 반영
-            temp_wl = self.window_level + self.deltaWL
-            temp_ww = self.window_width + self.deltaWW
+            # 기존의 self.window_level, self.window_width에서 움직인 만큼의 self.deltaWL, self.deltaWW를 더해줌
+            self.window_level = self.window_level + self.deltaWL
+            self.window_width = self.window_width + self.deltaWW
 
-            if temp_wl < 0:
-                temp_wl = 0
-            
-            if temp_ww < 0:
-                temp_ww = 0
-            
-            
-            #self.EntireImage 이미지전체를 조절해줘야함.
+            # 현재의 window_width의 값이 0보다 작아지면 0으로 설정
+            if self.window_width <= 0:
+                self.window_width = 0
+            elif self.window_width > 900:
+                self.window_width = 900
 
-            print("move: ", temp_wl, temp_ww)
+            if self.window_level < -250:
+                self.window_level = -250
+            elif self.window_level > 100:
+                self.window_level = 100
+
+            print ("window_level, width",self.window_level, self.window_width)
+            print ("delta_level, width",self.deltaWL, self.deltaWW)
+            self.refresh() 
 
     def mousePressEvent(self, event):
+        # 지현
         if event.buttons () == QtCore.Qt.LeftButton | QtCore.Qt.RightButton:
-            if self.LRClicked == False:
+            # 마우스 동시 클릭을 토글로 설정함. (mouseReleaseEvent 인식을 안함)
+            if self.LRClicked == False: # 
                 self.LRClicked = True
+                x = event.globalX()
+                y = event.globalY()
+
+                self.LRpoint = [x, y]
         
             else:
                 self.LRClicked = False
-                self.window_level = self.window_level + self.deltaWL
-                self.window_width = self.window_width + self.deltaWW
-
-                if self.window_level < 0:
-                    self.window_level = 0
-                if self.window_width < 0:
-                    self.window_width = 0
             
-                print("최종반영 ", self.window_level, self.window_width)
-                self.adjustedImage = self.AdjustPixelRange(self.EntireImage, self.window_level, self.window_width)
-
+            
             print("mousePressEvent")
-            print("Mouse 클릭한 좌표: x={0},y={1}".format(event.x(), event.y()))
-            x = event.globalX()
-            y = event.globalY()
-            self.LRpoint = [x, y] #기준점 셋업
-            
-            print('x = ', x)
-            print('y = ', y)
-
-    def mouseReleaseEvent(self, event): 
-
-        # e ; QMouseEvent 
-        # if event.buttons () == QtCore.Qt.LeftButton | QtCore.Qt.RightButton:
-        #     print('BUTTON RELEASE') 
-            # self.mousePressEvent(e.buttons())
-        print('BUTTON RELEASE')
-        # self.mouseButtonKind(e.buttons())
-        print("Click the left and right mouse button")
-        print("Mouse 뗀 좌표: x={0},y={1}".format(event.x(), event.y()))
-        x1 = event.x()
-        y1 = event.y()
-        print('x1 = ', x1)
-        print('y1 = ', y1)
-
+            print("Mouse 클릭한 글로벌 좌표: x={0},y={1}".format(event.globalX(), event.globalY()))
+        
+  
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyApp()
